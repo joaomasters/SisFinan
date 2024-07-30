15c2a0ef291140a2bfa3adcdac88783f3cfb6aa2
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2024-07-29 23:29:38 -0300

    Ajustes na criação do layout

--- frontend/sisfinan/src/app/app.component.ts
+++ frontend/sisfinan/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SiderComponent } from './components/sider/sider.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SiderComponent, ContentComponent, FooterComponent, NzLayoutModule, NzMenuModule, NzIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'sisfinan';
-}
+export class AppComponent { }
