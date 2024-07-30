0a4a5539248a02bb3123e275acd963d360ceb323
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2024-07-30 14:07:02 -0300

    Criação das rotas

--- frontend/sisfinan/src/app/app.routes.ts
+++ frontend/sisfinan/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContentComponent } from './components/content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },  // Aqui você pode querer verificar se HeaderComponent é o componente certo
+  { path: 'about', component: ContentComponent },  // Aqui você pode querer verificar se ContentComponent é o componente certo
+  { path: '**', component: DashboardComponent } // Rota para páginas não encontradas
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // Configura as rotas principais
+  exports: [RouterModule] // Permite que outros módulos acessem as rotas
+})
+export class AppRoutingModule {}
--- frontend/sisfinan/src/app/components/sider/sider.component.ts
+++ frontend/sisfinan/src/app/components/sider/sider.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-sider',
   standalone: true,
-  imports: [NzLayoutModule, NzMenuModule, NzIconModule],
+  imports: [NzLayoutModule, NzMenuModule, NzIconModule, RouterModule],
   templateUrl: './sider.component.html',
   styleUrl: './sider.component.scss'
 })
