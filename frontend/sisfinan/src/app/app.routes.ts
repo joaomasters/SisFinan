d345c20374d1150f75a0479026fe2eca849a5e70
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2024-08-01 14:28:05 -0300

    Ajuste nas routes e criação da page contas

--- frontend/sisfinan/src/app/app.routes.ts
+++ frontend/sisfinan/src/app/app.routes.ts
@@ -4,17 +4,13 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ContentComponent } from './components/content/content.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SiderComponent } from './components/sider/sider.component';
+import { ContasComponent } from './pages/contas/contas.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Página inicial é o Login
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: SiderComponent,
-    children: [
-      { path: '', component: DashboardComponent },
-      { path: 'contas', component: ContentComponent }
-    ]
-   },  // Aqui você pode querer verificar se HeaderComponent é o componente certo
-  { path: 'about', component: ContentComponent },  // Aqui você pode querer verificar se ContentComponent é o componente certo
+  { path: 'dashboard', component: SiderComponent },  // Aqui você pode querer verificar se HeaderComponent é o componente certo
+  { path: 'contas', component: ContasComponent },  // Aqui você pode querer verificar se ContentComponent é o componente certo
   { path: '**', component: LoginComponent } // Rota para páginas não encontradas
 ];
 
